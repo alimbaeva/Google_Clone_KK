7b46ccd549a78268146b011b7e44ae78af2dc15c
Asel <asel.alimbaeva.kk@gmail.com>
2022-02-04 19:45:31 +0300

    fix: route and navigate in the files Routes

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Routes as Routed, Route, Navigate } from 'react-router-dom';
 
 export const Routes = () => {
     return (
-        <div>
-            <h1>Routes</h1>
+        <div className='p-4 '>
+            <Routed >
+                <Route exact path='/' element={<Navigate replace to="/search" />}>
+                    {/* <Redirect to='/search' /> */}
+                    {/* <Navigate replace to="/search" /> */}
+                    {/* <Route path="/" element={<Navigate replace to="/home" />} /> */}
+                </Route>
+            </Routed >
         </div>
     );
 };
