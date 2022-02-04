2c7c68be822256e07a43980577572d798203606b
Asel <asel.alimbaeva.kk@gmail.com>
2022-02-04 16:23:01 +0300

    feat: dark and light background

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ const App = () => {
     const [darkTheme, setDarkTheme] = useState(false);
     return (
         <div className={darkTheme ? 'dark' : ''}>
-            <div className='bg-gray-100 dark:bg-gray-900 dark:taxt-gray-200 min-h-screen'>
-                <Navbar />
+            <div className='bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen'>
+                <Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme} />
                 <Routes />
                 <Footer />
             </div>
