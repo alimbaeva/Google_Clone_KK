50c773c70faa3c9f0ba7280f1ee4b74c682fcd4d
Asel <asel.alimbaeva.kk@gmail.com>
2022-02-04 20:24:28 +0300

    fix: route path for Results file

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Routes as Routed, Route, Navigate } from 'react-router-dom';
 
+import { Results } from './Results';
+
 export const Routes = () => {
     return (
         <div className='p-4 '>
             <Routed >
-                <Route exact path='/' element={<Navigate replace to="/search" />}>
-                    {/* <Redirect to='/search' /> */}
-                    {/* <Navigate replace to="/search" /> */}
-                    {/* <Route path="/" element={<Navigate replace to="/home" />} /> */}
-                </Route>
+                <Route exact path='/' element={<Navigate replace to="/search" />} />
+                <Route exact path='/search' element={<Results />} />
+                <Route exact path='/images' element={<Results />} />
+                <Route exact path='/news' element={<Results />} />
+                <Route exact path='/videos' element={<Results />} />
+                {/* <Route exact path={['/search', '/images', '/news', '/videos']} element={<Results replace to={['/search', '/images', '/news', '/videos']} />} /> */}
+                {/* <Redirect to='/search' /> */}
+                {/* <Navigate replace to="/search" /> */}
+                {/* <Route path="/" element={<Navigate replace to="/home" />} /> */}
             </Routed >
         </div>
     );
