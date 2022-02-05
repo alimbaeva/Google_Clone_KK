c0c8b0f9f0ae89784136d630acd6f46d6c6b8eb1
Asel <asel.alimbaeva.kk@gmail.com>
2022-02-05 06:00:07 +0300

    feat: search location path

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -8,11 +8,44 @@ export const Results = () => {
     const { results, isLoading, getResults, searchTerm } = useResultContext();
     const location = useLocation();
 
+    useEffect(() => {
+        getResults('/search/q=elon+musk')
+    }, [])
+
     if (isLoading) return <Loading />;
     console.log(location.pathname);
 
     switch (location.pathname) {
         case '/search':
+            return (
+                <div className='flex flex-wrap justify-between spase-y-6 sm:px-56'>
+                    {results?.results?.map(({ link, title }, index) => (
+                        <div key={index} className='md:w-2/5 w-full'>
+                            <a href={link} target='_blank' rel='noreferrer'>
+                                <p className='text-sm'>
+                                    {link.length > 30 ? link.substring(0, 30) : link}
+                                </p>
+                                <p className='text-lg hover:underline dark:text-blue-300 text-blue-700'>
+                                    {title}
+                                </p>
+                            </a>
+                        </div>
+                    ))}
+                </div>
+            );
+        case '/images':
+            return (
+                <div className='flex flex-wrap justify-center items-center'>
+                    {results?.image_results?.map(({ image, link: { href, title } }, index) => (
+                        <a className='sm:p-3 p-5' href={href} key={index} target='_blank' rel='noreferrer'>
+
+                        </a>
+                    ))}
+                </div>
+            );
+        case '/news':
+            return 'SEARCH';
+        case '/videos':
             return 'SEARCH';
         default:
             return 'ERROR!';
